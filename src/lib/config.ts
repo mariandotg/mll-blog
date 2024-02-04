f88e6b7a22381126988d967fadfe8a931b3eadc1
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-04 17:19:52 -0300

    Added uri on dbCredentials

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -11,7 +11,8 @@ const dbCredentialsSchema = z.object({
   name: z.string().min(1),
   username: z.string().min(1),
   password: z.string().min(1),
-  port: z.string().min(1),
+  port: z.number().nonnegative(),
+  uri: z.string().min(1),
 });
 
 const DATABASE_HOST = getDbCredential('HOST');
@@ -19,18 +20,20 @@ const DATABASE_NAME = getDbCredential('NAME');
 const DATABASE_USERNAME = getDbCredential('USERNAME');
 const DATABASE_PASSWORD = getDbCredential('PASSWORD');
 const DATABASE_PORT = Number(getDbCredential('PORT'));
+const DATABASE_URI = `mysql://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}/${DATABASE_NAME}?ssl={"rejectUnauthorized":true}`;
 
 const env = envSchema.parse(process.env);
 
 /**
  * Database validated credentials
  */
-export const dbCrendentials = dbCredentialsSchema.parse({
+export const dbCredentials = dbCredentialsSchema.parse({
   host: DATABASE_HOST,
   name: DATABASE_NAME,
   username: DATABASE_USERNAME,
   password: DATABASE_PASSWORD,
   port: DATABASE_PORT,
+  uri: DATABASE_URI,
 });
 
 export const { NEXTAUTH_SECRET, NEXTAUTH_URL } = env;
