dfc45651d7c75b9329e0584b1e35b4bf9f6cf4da
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-25 18:50:26 -0300

    Added base usage of db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import { getDB } from '@/lib/db';
+import { articles } from '@/lib/db/schema';
 import Image from 'next/image';
 
-export default function Home() {
+export default async function Home() {
+  const db = await getDB();
+  const a = await db.drizzle.select().from(articles);
+  console.log(a);
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
