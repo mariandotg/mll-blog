fbb25ac13ed10aeffd3000073cb63ea311ea2dee
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-02-25 21:59:23 -0300

    Fixed Inter font issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700', '900'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
